646cfa8bdda05809df8b2979d4d7adda2d0b4678
Vit Pelekhaty <vitpelekhaty@gmail.com>
2020-04-02 12:40:14 +0700

    [+] подключение "движка" к базе данных MS SQL Server [+] методы для работы с логгером и опциями объекта Engine в пакете 	cmd/engine/sqlserver

--- cmd/engine/sqlserver/engine.go
+++ cmd/engine/sqlserver/engine.go
@@ -1,7 +1,10 @@
 package sqlserver
 
 import (
+	"context"
 	"database/sql"
+
+	_ "github.com/denisenkom/go-mssqldb"
 
 	"github.com/vitpelekhaty/dbmill-cli/internal/pkg/dir"
 	"github.com/vitpelekhaty/dbmill-cli/internal/pkg/filter"
@@ -19,7 +22,25 @@ type Engine struct {
 
 // NewEngine возвращает экземпляр Engine
 func NewEngine(connection string) (*Engine, error) {
-	return nil, nil
+	db, err := sql.Open("sqlserver", connection)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.PingContext(context.Background())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Engine{
+		db:      db,
+		logger:  nil,
+		include: nil,
+		exclude: nil,
+		output:  dir.Default,
+	}, nil
 }
 
 // SetLogger устанавливает логгер событий
@@ -40,5 +61,51 @@ func (self *Engine) SetExcludedObjects(filter filter.IFilter) {
 
 // SetOutputDirectoryStructure устанавливает описание структуры каталога, где будут созданы скрипты
 func (self *Engine) SetOutputDirectoryStructure(dirStruct dir.IStructure) {
-	self.output = dirStruct
+	if dirStruct == nil {
+		self.output = dir.Default
+	} else {
+		self.output = dirStruct
+	}
+}
+
+// Log создает запись в логе, если указан логгер
+func (self *Engine) Log(level log.Level, args ...interface{}) {
+	if self.logger == nil {
+		return
+	}
+
+	self.logger.Print(level, args...)
+}
+
+// Logf создает фомрматированную запись в логе, если указан логгер
+func (self *Engine) Logf(level log.Level, format string, args ...interface{}) {
+	if self.logger == nil {
+		return
+	}
+
+	self.logger.Printf(level, format, args...)
+}
+
+// Included проверяет, должен ли объект object быть включен в обработку
+func (self *Engine) Included(object string) error {
+	if self.include == nil {
+		return nil
+	}
+
+	return self.include.Match(object)
+}
+
+// Excluded проверяет, должен ли объект object быть исключен из обработки
+func (self *Engine) Excluded(object string) error {
+	if self.exclude == nil {
+		return filter.ErrorNotMatched
+	}
+
+	return self.exclude.Match(object)
+}
+
+// OutputDirectoryItemInfo возвращает целевой каталог и маску имени файла для указанного типа объекта itemType.
+// Если информация не найдена, то в параметре ok возвращается false, в противном случае - true
+func (self *Engine) OutputDirectoryItemInfo(item dir.StructItemType) (subdirectory, mask string, ok bool) {
+	return self.output.ItemInfo(item)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vitpelekhaty/dbmill-cli
 go 1.14
 
 require (
+	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/cobra v0.0.7
 	gopkg.in/yaml.v2 v2.2.2
